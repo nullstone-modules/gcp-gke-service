999c41cf4d9ce2a1a1be09a776085ac5065f8f8a
Brad Sickles <brad.sickles@gmail.com>
2021-12-29 15:20:57 -0500

    add secrets to capabilities definition

--- capabilities.tf
+++ capabilities.tf
@@ -8,6 +8,13 @@ locals {
       }
     ]
 
+    secrets = [
+      {
+        name  = ""
+        value = ""
+      }
+    ]
+
     // private_urls follows a wonky syntax so that we can send all capability outputs into the merge module
     // Terraform requires that all members be of type list(map(any))
     // They will be flattened into list(string) when we output from this module
