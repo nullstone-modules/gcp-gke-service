3bf99cb8a32c486589fd4d21893e6b8c17562fcf
Brad Sickles <brad.sickles@gmail.com>
2021-12-29 13:46:18 -0500

    clean

--- nullstone.tf
+++ nullstone.tf
@@ -13,7 +13,17 @@ data "ns_connection" "cluster" {
   type = "cluster/gcp-gke"
 }
 
+resource "random_string" "resource_suffix" {
+  length  = 5
+  lower   = true
+  upper   = false
+  number  = false
+  special = false
+}
+
 locals {
+  service_image          = var.service_image != "" ? var.service_image : data.google_container_registry_image.this.image_url
   cluster_endpoint       = data.ns_connection.cluster.outputs.cluster_endpoint
   cluster_ca_certificate = data.ns_connection.cluster.outputs.cluster_ca_certificate
 }
+
