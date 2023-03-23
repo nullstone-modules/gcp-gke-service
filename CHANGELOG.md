87d70138932025a0a75d2ec1f40406748a5b330e
Brad Sickles <brad.sickles@gmail.com>
2023-03-22 21:11:57 -0400

    Fix kubernetes provider host

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
-# 0.4.0 (Unreleased)
+# 0.4.1 (Mar 22, 2023)
+* Fix kubernetes provider host.
+
+# 0.4.0 (Mar 22, 2023)
 * Added support for environment variable interpolation.
 * Renamed variables to drop `service_` prefix.
 * Upgraded `capabilities.tf.tmpl` to fix various bugs with variable nil values and capability management.
--- kubernetes.tf
+++ kubernetes.tf
@@ -1,7 +1,7 @@
 data "google_client_config" "provider" {}
 
 provider "kubernetes" {
-  host                   = local.cluster_endpoint
+  host                   = "https://${local.cluster_endpoint}"
   token                  = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
 }
