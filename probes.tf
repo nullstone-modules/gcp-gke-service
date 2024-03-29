a645d040e9a4aba88b16ef7a682e52047f1a6b64
Brad Sickles <brad.sickles@gmail.com>
2024-03-29 13:09:52 -0400

    Fixed syntax in probes calc

--- probes.tf
+++ probes.tf
@@ -5,44 +5,44 @@ locals {
 
   startup_probes = [
     for p in local.cap_startup_probes : {
-      initial_delay_seconds = lookup(sp.value, "initial_delay")
-      period_seconds        = lookup(sp.value, "period")
-      timeout_seconds       = lookup(sp.value, "timeout")
-      success_threshold     = lookup(sp.value, "success_threshold")
-      failure_threshold     = lookup(sp.value, "failure_threshold")
+      initial_delay_seconds = lookup(p, "initial_delay")
+      period_seconds        = lookup(p, "period")
+      timeout_seconds       = lookup(p, "timeout")
+      success_threshold     = lookup(p, "success_threshold")
+      failure_threshold     = lookup(p, "failure_threshold")
 
-      exec       = compact([lookup(sp.value, "exec", "null")])
-      grpc       = compact([lookup(sp.value, "grpc", "null")])
-      http_get   = compact([lookup(sp.value, "http_get", "null")])
-      tcp_socket = compact([lookup(sp.value, "tcp_socket", "null")])
+      exec       = compact([lookup(p, "exec", "null")])
+      grpc       = compact([lookup(p, "grpc", "null")])
+      http_get   = compact([lookup(p, "http_get", "null")])
+      tcp_socket = compact([lookup(p, "tcp_socket", "null")])
     }
   ]
   readiness_probes = [
     for p in local.cap_readiness_probes : {
-      initial_delay_seconds = lookup(sp.value, "initial_delay")
-      period_seconds        = lookup(sp.value, "period")
-      timeout_seconds       = lookup(sp.value, "timeout")
-      success_threshold     = lookup(sp.value, "success_threshold")
-      failure_threshold     = lookup(sp.value, "failure_threshold")
+      initial_delay_seconds = lookup(p, "initial_delay")
+      period_seconds        = lookup(p, "period")
+      timeout_seconds       = lookup(p, "timeout")
+      success_threshold     = lookup(p, "success_threshold")
+      failure_threshold     = lookup(p, "failure_threshold")
 
-      exec       = compact([lookup(sp.value, "exec", "null")])
-      grpc       = compact([lookup(sp.value, "grpc", "null")])
-      http_get   = compact([lookup(sp.value, "http_get", "null")])
-      tcp_socket = compact([lookup(sp.value, "tcp_socket", "null")])
+      exec       = compact([lookup(p, "exec", "null")])
+      grpc       = compact([lookup(p, "grpc", "null")])
+      http_get   = compact([lookup(p, "http_get", "null")])
+      tcp_socket = compact([lookup(p, "tcp_socket", "null")])
     }
   ]
   liveness_probes = [
     for p in local.cap_liveness_probes : {
-      initial_delay_seconds = lookup(sp.value, "initial_delay")
-      period_seconds        = lookup(sp.value, "period")
-      timeout_seconds       = lookup(sp.value, "timeout")
-      success_threshold     = lookup(sp.value, "success_threshold")
-      failure_threshold     = lookup(sp.value, "failure_threshold")
+      initial_delay_seconds = lookup(p, "initial_delay")
+      period_seconds        = lookup(p, "period")
+      timeout_seconds       = lookup(p, "timeout")
+      success_threshold     = lookup(p, "success_threshold")
+      failure_threshold     = lookup(p, "failure_threshold")
 
-      exec       = compact([lookup(sp.value, "exec", "null")])
-      grpc       = compact([lookup(sp.value, "grpc", "null")])
-      http_get   = compact([lookup(sp.value, "http_get", "null")])
-      tcp_socket = compact([lookup(sp.value, "tcp_socket", "null")])
+      exec       = compact([lookup(p, "exec", "null")])
+      grpc       = compact([lookup(p, "grpc", "null")])
+      http_get   = compact([lookup(p, "http_get", "null")])
+      tcp_socket = compact([lookup(p, "tcp_socket", "null")])
     }
   ]
 }
