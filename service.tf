94ffb4b56e09d1c53f11b46df8819348eaf17412
Brad Sickles <brad.sickles@gmail.com>
2026-02-24 13:11:21 -0500

    Allow capabilities to annotate deployment/service (#18)

--- service.tf
+++ service.tf
@@ -1,23 +1,26 @@
 locals {
   has_service  = var.service_port == 0 || var.container_port == 0 ? false : true
   service_name = !local.has_service ? "" : local.app_name
+
+  base_service_annotations = tomap({
+    "cloud.google.com/neg" = jsonencode({ ingress : true })
+  })
+  cap_service_annotations = tomap({ for ann in local.capabilities.service_annotations : ann.name => ann.value })
+  service_annotations     = merge(local.base_service_annotations, local.cap_service_annotations)
 }
 
 resource "kubernetes_service_v1" "this" {
   count = local.has_service ? 1 : 0
 
   lifecycle {
-    ignore_changes = [metadata[0].annotations]
+    ignore_changes = [metadata[0].annotations["cloud.google.com/neg"]]
   }
 
   metadata {
-    name      = local.service_name
-    namespace = local.app_namespace
-    labels    = local.app_labels
-
-    annotations = {
-      "cloud.google.com/neg" = jsonencode({ ingress : true })
-    }
+    name        = local.service_name
+    namespace   = local.app_namespace
+    labels      = local.app_labels
+    annotations = local.service_annotations
   }
 
   spec {
